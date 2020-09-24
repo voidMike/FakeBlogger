eb799fd3d73d36d35ddd7f3a36f967a1edf1e6e9
voidMike <m.milosavjevic97@gmail.com>
2020-09-24 18:36:15 +0200

    Added: Basic groundwork for displaying posts.

---
+++ src/components/Blog.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <blog-post
+      v-for="post in posts"
+      :key = "post.postID"
+      :userID = "post.userID"
+      :postID = "post.postID"
+      :postTitle = post.postTitle
+      :postBody = "post.postBody"
+    ></blog-post>
+  </div>
+</template>
+
+<script>
+import BlogPost from "@/components/BlogPost";
+export default {
+  name: "Blog",
+  components:{BlogPost},
+  data: function(){
+    return {
+      posts:[
+        {userID:1, postID:0, postTitle: "Title 0", postBody:"Hello World."},
+        {userID: 1,postID: 1, postTitle: "Big Day Today, Freeman", postBody: "Do you know who ate all the doughnuts?"},
+        {userID: 1,postID: 2, postTitle: "Unpopular Opinion", postBody: "Why do we all have to wear these ridiculous ties?"}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+   <div>User {{userID}}</div>
+   <div>Post#{{postID}}</div>
+   <div>{{postTitle}}</div>
+   <div>{{postBody}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "BlogPost",
+  props:{
+    userID: Number,
+    postID: Number,
+    postTitle: String,
+    postBody: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
