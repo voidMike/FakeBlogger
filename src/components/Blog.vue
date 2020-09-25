1c8f8f869ff181f33b5abd7392d29c460be01e22
voidMike <m.milosavjevic97@gmail.com>
2020-09-25 07:52:59 +0200

    Added: Non-networked post deletion.

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="blog">
-    <blog-post
-      v-for="post in posts"
-      :key = "post.id"
-      :userId = "post.userId"
-      :id = "post.id"
-      :title = "post.title"
-      :body = "post.body"
-    ></blog-post>
+    <div v-if="loaded" class="post-wrapper">
+      <blog-post
+        v-on:blog-post-delete="blogPostDelete"
+        v-for="post in posts"
+        :key = "post.id"
+        :userId = "post.userId"
+        :id = "post.id"
+        :title = "post.title"
+        :body = "post.body"
+      ></blog-post>
+    </div>
+    <div v-if="!loaded">
+      Loading Posts...
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -19,16 +26,31 @@ export default {
   data: function(){
     return {
       posts:[
-        {userID:1, postID:0, postTitle: "Title 0", postBody:"Hello World."},
-        {userID: 1,postID: 1, postTitle: "Big Day Today, Freeman", postBody: "Do you know who ate all the doughnuts?"},
-        {userID: 1,postID: 2, postTitle: "Unpopular Opinion", postBody: "Why do we all have to wear these ridiculous ties?"}
-      ]
+        {userId:1, id:0, title: "Title 0", body:"Hello World."}
+      ],
+      loaded:false
     }
   },
   // This works for now. Maybe one day we can lazy load, but (ironically) I'm too lazy to implement this.
   mounted: function (){
     fetch("https://jsonplaceholder.typicode.com/posts").then(response=>response.json())
-        .then(json=>{this.posts=json;});
+        .then(json=>{this.posts=json; this.loaded=true;});
+  },
+  methods:{
+    search: function(){
+
+    },
+    getPostIndexByID: function (id){
+
+      let index = this.posts.findIndex(
+          (elem)=>{return elem.id === id; }
+      );
+      console.log(index);
+      return index;
+    },
+    blogPostDelete: function (id){
+      this.posts.splice(this.getPostIndexByID(id),1);
+    }
   }
 }
 </script>
