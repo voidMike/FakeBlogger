43b213d22fa2606314c918ad7bedf030f756b610
voidMike <m.milosavjevic97@gmail.com>
2020-09-25 07:43:11 +0200

    Added: Non-networked post deletion. Few cosmetic changes.

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="blog-post">
-   <div class="user-id">User {{userId}}</div>
-   <div class="post-id">Post#{{id}}</div>
+   <div class="blog-post-header">
+     <div class="user-id">User {{userId}}</div>
+     <div class="post-id">Post#{{id}}</div>
+   </div>
    <div class="blog-title">{{title}}</div>
    <div class="blog-body">{{body}}</div>
+   <div class="blog-controls">
+     <button @click="$emit('blog-post-delete',id)"> Delete </button>
+   </div>
   </div>
 </template>
 
@@ -26,9 +31,11 @@ export default {
   color:white;
   max-width:600px;
   border:3px solid #42b983;
+  margin-bottom: 10px;
 }
 .blog-body{
-  border: 1px solid #42b983;
+  border-bottom: 1px solid #42b983;
+  border-top:1px solid #42b983;
   text-align: left;
 }
 .blog-title{
@@ -36,7 +43,15 @@ export default {
 }
 .user-id,.post-id{
   display: inline-block;
-
+  padding:5px;
+  padding-left: 2px;
+}
+.post-id{
+  color: #7c8ea0;
+}
+.blog-post-header{
+  display:flex;
+  justify-content: space-between;
 }
 
 </style>
